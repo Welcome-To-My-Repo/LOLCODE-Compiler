18d60f1690be937187084bdcca9f464f54a788f3
o_o <smithtrombone@gmail.com>
2018-03-11 17:31:25 -0400

    added main.cpp and error definitions

---
+++ err.h
@@ -0,0 +1,8 @@
+#ifndef errors
+#define errors
+
+#define NO_NOOB_TO_YARN "Cannot cast NOOB to YARN"
+#define NO_NOOB_TO_NUMBR "Cannot cast NOOB to NUMBR"
+#define NO_NOOB_TO_NUMBAR "Cannot cast NOOB to NUMBAR"
+
+#endif
